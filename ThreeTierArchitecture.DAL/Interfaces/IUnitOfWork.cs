2ad3c8c6f67212fb659c89eea5edd8e0e6c980f8
Sergey V. <letsdrumm@gmail.com>
2020-01-14 15:11:41 +0300

    Полное описание Data Access Layer

---
+++ ThreeTierArchitecture.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ThreeTierArchitecture.DAL.Entities;
+
+namespace ThreeTierArchitecture.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Phone> Phones { get; }
+        IRepository<Order> Orders { get; }
+        void Save();
+    }
+}
